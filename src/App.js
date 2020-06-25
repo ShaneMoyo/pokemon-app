ab10a6e71e5c7d4c484abfad38e728d66bbbf8be
shanemoyo <smoyo@pdx.edu>
2020-06-25 08:38:13 -0700

    add routing and about page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import PokemonContainer from './components/pokemon'
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Link, 
+} from 'react-router-dom'; 
 
 function App() {
+  const About = () => (
+    <section> 
+      <h1>Pokemon App</h1> 
+      <p>
+        Welcome to my pokemon app. This application wraps the pokemon api.
+        Have fun looking up different pokemon.  
+      </p>
+    </section> 
+  );
   return (
     <div className="App">
-      <PokemonContainer />
+      <Router> 
+        <nav>
+          <Link to='/'>Home</Link> 
+          <Link to='/about'>About</Link> 
+        </nav> 
+        <Switch>
+          <Route path='/about' render={() => <About/>} />
+          <Route path='/' component={PokemonContainer} />
+        </Switch> 
+      </Router> 
     </div>
   );
 }
