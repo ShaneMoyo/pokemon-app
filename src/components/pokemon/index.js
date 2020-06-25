2fd6348f5cbcad1447f3c5a71745ae82a84ba558
shanemoyo <smoyo@pdx.edu>
2020-06-25 08:31:46 -0700

    Add pokemon container.

---
+++ src/components/pokemon/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+
+export default class PokemonContainer extends Component { 
+
+    render() { 
+        return (
+            <section>
+                <h1>Pokemon Container</h1> 
+            </section>
+        ); 
+         
+    }
+}
